b20db8e320d010f6066806c3d376d0ab7cd47cbb
Ali <lanfcbarca1105@gmail.com>
2024-01-06 15:59:40 +0700

    done full

---
+++ ProjectSem3/ProjectSem3/Controllers/OrdersController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectSem3.Model;
+
+namespace ProjectSem3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ShopDbContext _context;
+
+        public OrdersController(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+          if (_context.Orders == null)
+          {
+              return NotFound();
+          }
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+          if (_context.Orders == null)
+          {
+              return NotFound();
+          }
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        // PUT: api/Orders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrder(int id, Order updatedOrder)
+        {
+            try
+            {
+                if (id != updatedOrder.OrderID)
+                {
+                    return BadRequest();
+                }
+
+                var existingOrder = await _context.Set<Order>().FindAsync(id);
+
+                if (existingOrder == null)
+                {
+                    return NotFound();
+                }
+
+                existingOrder.CartID = updatedOrder.CartID;
+                existingOrder.Total = updatedOrder.Total;
+                existingOrder.Status = updatedOrder.Status;
+                existingOrder.DeliveryType = updatedOrder.DeliveryType;
+                existingOrder.LastUpdateAt = DateTime.Now;
+
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        // POST: api/Orders
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            try
+            {
+                order.CreateAt = DateTime.Now;
+                _context.Set<Order>().Add(order);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetOrder), new { id = order.OrderID }, order);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            if (_context.Orders == null)
+            {
+                return NotFound();
+            }
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderExists(int id)
+        {
+            return (_context.Orders?.Any(e => e.OrderID == id)).GetValueOrDefault();
+        }
+    }
+}
